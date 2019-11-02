e7d0aa8874b9444fc1b4b81f502515c5d0cc0c07
Aleksei Danilov <danilovdev@gmail.com>
2019-11-02 16:44:00 +0800

    initial commit

---
+++ DeclarativeUI/DeclarativeUI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DeclarativeUI
+//
+//  Created by Alexey Danilov on 02.11.2019.
+//  Copyright © 2019 DanilovDev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
